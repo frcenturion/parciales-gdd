64efd6f62d2cd5b03ab8aea43cabb0b477c16ba4
Franco Centurion <frcenturion@frba.utn.edu.ar>
2024-11-16 10:59:19 -0300

    Agrego parciales rendidos

--- parcial-2C2024/parcial_sql.sql
+++ parcial-2C2024/parcial_sql.sql
@@ -4,8 +4,48 @@
     Curso: K3673
     Alumno: Franco Ezequiel Centurión
     Profesor: Edgardo Lacquaniti
+    Legajo: 1780189
 */
 
+SELECT
+    ROW_NUMBER() over (ORDER BY SUM(if1.item_cantidad) DESC) as numero_fila,
+    c1.clie_codigo as codigo_cliente,
+    c1.clie_razon_social as nombre_cliente,
 
+    SUM(if1.item_cantidad) as cantidad_total_comprada,
 
+    (
+        SELECT TOP 1
+            r1.rubr_detalle     -- Categoria más comprada (agarre el detalle pero también podría haber agarrado el id)
+        FROM Factura f2
+            JOIN Item_Factura if2 ON f2.fact_tipo = if2.item_tipo and f2.fact_sucursal = if2.item_sucursal and f2.fact_numero = if2.item_numero
+            JOIN Producto p1 ON if2.item_producto = p1.prod_codigo
+            JOIN Rubro r1 ON p1.prod_rubro = r1.rubr_id
+        WHERE f2.fact_cliente = c1.clie_codigo AND YEAR(f2.fact_fecha) = 2012
+        GROUP BY r1.rubr_id, r1.rubr_detalle
+        ORDER BY SUM(if2.item_cantidad)
+
+    ) as categoria_mas_comprada_2012
+
+FROM Cliente c1
+    JOIN Factura f1 ON c1.clie_codigo = f1.fact_cliente
+    JOIN Item_Factura if1 ON f1.fact_tipo = if1.item_tipo and f1.fact_sucursal = if1.item_sucursal and f1.fact_numero = if1.item_numero
+GROUP BY c1.clie_codigo, c1.clie_razon_social
+HAVING
+    (
+        SELECT COUNT(DISTINCT r2.rubr_id)
+        FROM Rubro r2
+            JOIN Producto p2 ON r2.rubr_id = p2.prod_rubro
+            JOIN Item_Factura if2 ON p2.prod_codigo = if2.item_producto
+            JOIN Factura f2 ON if2.item_tipo = f2.fact_tipo and if2.item_sucursal = f2.fact_sucursal and if2.item_numero = f2.fact_numero
+        WHERE f2.fact_cliente = c1.clie_codigo AND YEAR(f2.fact_fecha) = 2012
+
+        ) > 3       -- Compro en más de 3 rubros diferentes en el 2012
+        AND
+        NOT EXISTS( SELECT                      -- No existe una factura de ese cliente que haya sido emitida en un año impar (o sea, todas las compras fueron en años pares)
+                        1
+                    FROM Factura f3
+                    WHERE f3.fact_cliente = c1.clie_codigo AND YEAR(f3.fact_fecha) % 2 != 0)
+
+ORDER BY SUM(if1.item_cantidad) DESC
 
--- parcial-2C2024/parcial_tsql.sql
+++ parcial-2C2024/parcial_tsql.sql
@@ -4,5 +4,82 @@
     Curso: K3673
     Alumno: Franco Ezequiel Centurión
     Profesor: Edgardo Lacquaniti
+    Legajo: 1780189
 */
+
+DROP TABLE diez_productos_mas_vendidos_por_anio
+
+CREATE TABLE diez_productos_mas_vendidos_por_anio (
+    anio INT,
+    producto_codigo CHAR(8)
+)
+
+DROP PROCEDURE pr_productos_mas_vendidos
+
+CREATE PROCEDURE pr_productos_mas_vendidos
+AS
+BEGIN
+    -- Declaro un cursor para recorrerme todos los años del sistema
+    DECLARE cur_anios CURSOR FOR
+        SELECT
+            DISTINCT(YEAR(fact_fecha))
+        FROM Factura
+        ORDER BY YEAR(fact_fecha)
+
+    -- Declaro una variable para captar la info del cursor de los anios
+    DECLARE @anio INT
+
+    -- Declaro una variable para traerme los productos que va recorriendo el cursor
+    DECLARE @producto CHAR(8)
+
+
+    -- Primero abrimos el cursor para recorrer los años
+    OPEN cur_anios
+    FETCH cur_anios INTO @anio
+    WHILE @@fetch_status = 0
+        BEGIN
+           -- Ahora por cada año tenemos que buscarnos los 10 productos más vendidos e ir insertándolos en nuestra tabla
+
+        -- Declaro un cursor para traerme los 10 productos mas vendidos en un año especifico (lo traigo de mayor a menor)
+            DECLARE cur_productos_mas_vendidos_por_anio CURSOR FOR
+                SELECT TOP 10
+                    if1.item_producto
+                FROM Item_Factura if1
+                    JOIN Factura f1 ON if1.item_tipo = f1.fact_tipo and if1.item_sucursal = f1.fact_sucursal and if1.item_numero = f1.fact_numero
+                WHERE YEAR(f1.fact_fecha) = @anio
+                GROUP BY if1.item_producto
+                ORDER BY SUM(if1.item_cantidad) DESC
+
+
+            OPEN cur_productos_mas_vendidos_por_anio
+            FETCH cur_productos_mas_vendidos_por_anio INTO @producto
+            WHILE @@fetch_status = 0
+                BEGIN
+
+                    -- Vamos insertando cada uno de los productos más vendidos
+                    INSERT INTO diez_productos_mas_vendidos_por_anio (anio, producto_codigo)
+                        VALUES (@anio,  @producto)
+
+                    FETCH NEXT FROM cur_productos_mas_vendidos_por_anio INTO @producto
+                END
+
+            -- Cerramos el cursor y en la proxima vuelta abriremos uno con los productos del año siguiente
+            CLOSE cur_productos_mas_vendidos_por_anio
+            DEALLOCATE cur_productos_mas_vendidos_por_anio
+
+            FETCH NEXT FROM cur_anios INTO @anio
+        END
+    CLOSE cur_anios
+    DEALLOCATE cur_anios
+END
+
+EXEC pr_productos_mas_vendidos
+
+-- Chequeo
+SELECT * FROM diez_productos_mas_vendidos_por_anio
+
+
+
+
+
 
